e9bfbb154574e9e3667c4b4946fee445f617036b
Ibrahim Aden <ibrahimaden@Ibrahims-MacBook-Air.local>
2017-01-15 14:30:50 -0800

    Initial Commit

---
+++ Chuck taylor StoreTests/Chuck_taylor_StoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chuck_taylor_StoreTests.swift
+//  Chuck taylor StoreTests
+//
+//  Created by Ibrahim Aden on 1/15/17.
+//  Copyright © 2017 Ibrahim Aden. All rights reserved.
+//
+
+import XCTest
+@testable import Chuck_taylor_Store
+
+class Chuck_taylor_StoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
